8e03f99e4c76f96bd00157891851e1a38e3b889f
greg <gregory.diamos@gmail.com>
2021-12-01 02:42:31 -0800

    ENH: Header logo

--- mlops_platform/react_webserver/src/components/Header.js
+++ mlops_platform/react_webserver/src/components/Header.js
@@ -3,11 +3,9 @@ import { makeStyles } from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Logout from './login/Logout'
-
+import SmallWave from './logos/wave-small.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,7 +27,7 @@ export default function Header(props) {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <img src={SmallWave} height="60" alt="wave" />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             DataWav
